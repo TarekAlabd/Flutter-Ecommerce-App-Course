d65393189ec7c6570d437178807ededf882f672e
Tarek Alabd <tarekalabd1@gmail.com>
2023-12-05 23:37:58 +0200

    feat: building categories tab view

--- lib/views/widgets/category_tab_view.dart
+++ lib/views/widgets/category_tab_view.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/models/category_model.dart';
 
 class CategoryTabView extends StatelessWidget {
   const CategoryTabView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Category Tab View'),);
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final category = dummyCategories[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: InkWell(
+            onTap: () {},
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                color: category.bgColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 32,
+                  horizontal: 16,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      category.name,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: category.textColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                    ),
+                    Text(
+                      '${category.productsCount} Products',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                            color: category.textColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: dummyCategories.length,
+    );
   }
 }
