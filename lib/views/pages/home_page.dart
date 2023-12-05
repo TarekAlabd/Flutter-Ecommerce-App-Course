44396c9c24ae0f2a7f0099eddc2f7037789e4346
Tarek Alabd <tarekalabd1@gmail.com>
2023-12-05 23:16:41 +0200

    feat: adding two tabs inside home page

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -3,10 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:flutter_ecommerce_app/models/home_carosel_item_model.dart';
 import 'package:flutter_ecommerce_app/models/product_item_model.dart';
+import 'package:flutter_ecommerce_app/utils/app_colors.dart';
+import 'package:flutter_ecommerce_app/views/widgets/category_tab_view.dart';
+import 'package:flutter_ecommerce_app/views/widgets/home_tab_view.dart';
 import 'package:flutter_ecommerce_app/views/widgets/product_item.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,116 +30,77 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 25,
-                          backgroundImage: CachedNetworkImageProvider(
-                              'https://pbs.twimg.com/profile_images/1493704582505144323/Stvh3FSK_400x400.jpg'),
-                        ),
-                        const SizedBox(width: 16.0),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Tarek Alabd',
-                              style: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            Text(
-                              'Let\'s go shopping!',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelSmall!
-                                  .copyWith(
-                                    color: Colors.grey,
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.search),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.notifications),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 24.0),
-                FlutterCarousel.builder(
-                  itemCount: dummyHomeCarouselItems.length,
-                  itemBuilder: (BuildContext context, int itemIndex,
-                          int pageViewIndex) =>
-                      Padding(
-                    padding: const EdgeInsetsDirectional.only(end: 16.0),
-                    child: CachedNetworkImage(
-                      imageUrl: dummyHomeCarouselItems[itemIndex].imgUrl,
-                      fit: BoxFit.fill,
-                      placeholder: (context, url) => const Center(
-                        child: CircularProgressIndicator.adaptive(),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 25,
+                        backgroundImage: CachedNetworkImageProvider(
+                            'https://pbs.twimg.com/profile_images/1493704582505144323/Stvh3FSK_400x400.jpg'),
                       ),
-                      errorWidget: (context, url, error) => const Icon(
-                        Icons.error,
-                        color: Colors.red,),
-                    ),
-                  ),
-                  options: CarouselOptions(
-                    height: 200,
-                    showIndicator: true,
-                    autoPlay: true,
-                    slideIndicator: CircularWaveSlideIndicator(),
-                  ),
-                ),
-                const SizedBox(height: 24.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'New Arrivals',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            fontWeight: FontWeight.w600,
+                      const SizedBox(width: 16.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Tarek Alabd',
+                            style: Theme.of(context).textTheme.labelLarge,
                           ),
-                    ),
-                    Text(
-                      'See All',
-                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.w600),
-                    ),
+                          Text(
+                            'Let\'s go shopping!',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall!
+                                .copyWith(
+                                  color: Colors.grey,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.notifications),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              TabBar(
+                controller: _tabController,
+                unselectedLabelColor: AppColors.grey,
+                tabs: const [
+                  Tab(
+                    text: 'Home',
+                  ),
+                  Tab(
+                    text: 'Category',
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    HomeTabView(),
+                    CategoryTabView(),
                   ],
                 ),
-                const SizedBox(height: 16.0),
-                GridView.builder(
-                  itemCount: dummyProducts.length,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 25,
-                    crossAxisSpacing: 10,
-                  ),
-                  itemBuilder: (context, index) {
-                    return ProductItem(
-                      productItem: dummyProducts[index],
-                    );
-                  },
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
