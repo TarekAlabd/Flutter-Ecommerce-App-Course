06b673fbd572dbcd560cdf62050502e2a131e76f
Tarek Alabd <tarekalabd1@gmail.com>
2023-12-25 16:52:47 +0200

    feat: adding the address and payment empty states

--- lib/views/pages/checkout_page.dart
+++ lib/views/pages/checkout_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/utils/app_colors.dart';
 import 'package:flutter_ecommerce_app/view_models/checkout_cubit/checkout_cubit.dart';
 import 'package:flutter_ecommerce_app/views/widgets/checkout_headlines_item.dart';
+import 'package:flutter_ecommerce_app/views/widgets/empty_shipping_payment.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({super.key});
@@ -40,137 +41,152 @@ class CheckoutPage extends StatelessWidget {
                 );
               } else if (state is CheckoutLoaded) {
                 final cartItems = state.cartItems;
-                return SingleChildScrollView(
-                    child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                  ),
-                  child: Column(
-                    children: [
-                      CheckoutHeadlinesItem(
-                        title: 'Address',
-                        onTap: () {},
-                      ),
-                      CheckoutHeadlinesItem(
-                        title: 'Products',
-                        numOfProducts: state.numOfProducts,
-                      ),
-                      const SizedBox(height: 16.0),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: cartItems.length,
-                        separatorBuilder: (context, index) {
-                          return Divider(
-                            color: AppColors.grey2,
-                          );
-                        },
-                        itemBuilder: (context, index) {
-                          final cartItem = cartItems[index];
-                          return Row(
-                            children: [
-                              DecoratedBox(
-                                decoration: BoxDecoration(
-                                  color: AppColors.grey2,
-                                  borderRadius: BorderRadius.circular(16),
+                return SafeArea(
+                  child: SingleChildScrollView(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                    ),
+                    child: Column(
+                      children: [
+                        CheckoutHeadlinesItem(
+                          title: 'Address',
+                          onTap: () {},
+                        ),
+                        const SizedBox(height: 16.0),
+                        const EmptyShippingAndPayment(
+                          title: 'Add shipping address',
+                        ),
+                        const SizedBox(height: 16.0),
+                        CheckoutHeadlinesItem(
+                          title: 'Products',
+                          numOfProducts: state.numOfProducts,
+                        ),
+                        const SizedBox(height: 16.0),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: cartItems.length,
+                          separatorBuilder: (context, index) {
+                            return Divider(
+                              color: AppColors.grey2,
+                            );
+                          },
+                          itemBuilder: (context, index) {
+                            final cartItem = cartItems[index];
+                            return Row(
+                              children: [
+                                DecoratedBox(
+                                  decoration: BoxDecoration(
+                                    color: AppColors.grey2,
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  child: CachedNetworkImage(
+                                    imageUrl: cartItem.product.imgUrl,
+                                    height: 125,
+                                    width: 125,
+                                  ),
                                 ),
-                                child: CachedNetworkImage(
-                                  imageUrl: cartItem.product.imgUrl,
-                                  height: 125,
-                                  width: 125,
-                                ),
-                              ),
-                              const SizedBox(width: 16.0),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      cartItem.product.name,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium,
-                                    ),
-                                    const SizedBox(height: 4.0),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text.rich(
-                                          TextSpan(
-                                            text: 'Size: ',
+                                const SizedBox(width: 16.0),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        cartItem.product.name,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium,
+                                      ),
+                                      const SizedBox(height: 4.0),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text.rich(
+                                            TextSpan(
+                                              text: 'Size: ',
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .titleMedium!
+                                                  .copyWith(
+                                                    color: AppColors.grey,
+                                                  ),
+                                              children: [
+                                                TextSpan(
+                                                  text: cartItem.size.name,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .titleMedium,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          Text(
+                                            '\$${cartItem.totalPrice.toStringAsFixed(1)}',
                                             style: Theme.of(context)
                                                 .textTheme
-                                                .titleMedium!
+                                                .headlineSmall!
                                                 .copyWith(
-                                                  color: AppColors.grey,
+                                                  fontWeight: FontWeight.bold,
                                                 ),
-                                            children: [
-                                              TextSpan(
-                                                text: cartItem.size.name,
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .titleMedium,
-                                              ),
-                                            ],
                                           ),
-                                        ),
-                                        Text(
-                                          '\$${cartItem.totalPrice.toStringAsFixed(1)}',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headlineSmall!
-                                              .copyWith(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                      CheckoutHeadlinesItem(title: 'Payment'),
-                      Divider(
-                        color: AppColors.grey2,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Total Amount',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .copyWith(
-                                  color: AppColors.grey,
-                                ),
-                          ),
-                          Text(
-                            '\$${state.totalAmount.toStringAsFixed(1)}',
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 40.0),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 60,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(context).primaryColor,
-                            foregroundColor: AppColors.white,
-                          ),
-                          child: const Text('Proceed to Buy'),
+                              ],
+                            );
+                          },
                         ),
-                      ),
-                    ],
-                  ),
-                ));
+                        const SizedBox(height: 16.0),
+                        const CheckoutHeadlinesItem(title: 'Payment Methods'),
+                        const SizedBox(height: 16.0),
+                        const EmptyShippingAndPayment(
+                          title: 'Add Payment Method',
+                        ),
+                        const SizedBox(height: 16.0),
+                        Divider(
+                          color: AppColors.grey2,
+                        ),
+                        const SizedBox(height: 16.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Amount',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: AppColors.grey,
+                                  ),
+                            ),
+                            Text(
+                              '\$${state.totalAmount.toStringAsFixed(1)}',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 40.0),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              foregroundColor: AppColors.white,
+                            ),
+                            child: const Text('Proceed to Buy'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+                );
               } else {
                 return const Center(
                   child: Text('Something went wrong!'),
---
+++ lib/views/widgets/empty_shipping_payment.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/utils/app_colors.dart';
+
+class EmptyShippingAndPayment extends StatelessWidget {
+  final String title;
+  const EmptyShippingAndPayment({super.key, required this.title,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.grey3,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 24.0,
+        ),
+        child: Column(
+          children: [
+            const Icon(
+              Icons.add,
+              size: 30,
+            ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
