694648fbdd39298aabe7f0615f28cefd75349a0d
Tarek Alabd <tarekalabd1@gmail.com>
2024-05-26 22:11:01 +0300

    feat: building create account page

--- lib/views/pages/register_page.dart
+++ lib/views/pages/register_page.dart
@@ -5,16 +5,17 @@ import 'package:flutter_ecommerce_app/views/widgets/label_with_textfield.dart';
 import 'package:flutter_ecommerce_app/views/widgets/main_button.dart';
 import 'package:flutter_ecommerce_app/views/widgets/social_media_button.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<RegisterPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -33,15 +34,22 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   const SizedBox(height: 50),
                   Text(
-                    'Login Account',
+                    'Create Account',
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
                   const SizedBox(height: 8),
                   Text(
-                    'Please, login with registered account!',
+                    'Start shopping with create your account!',
                     style: Theme.of(context).textTheme.labelLarge!.copyWith(
                           color: AppColors.grey,
                         ),
+                  ),
+                  const SizedBox(height: 24),
+                  LabelWithTextField(
+                    label: 'Username',
+                    controller: usernameController,
+                    prefixIcon: Icons.person,
+                    hintText: 'Enter you username',
                   ),
                   const SizedBox(height: 24),
                   LabelWithTextField(
@@ -63,17 +71,9 @@ class _LoginPageState extends State<LoginPage> {
                       onPressed: () {},
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text('Forgot Password'),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 40),
                   MainButton(
-                    text: 'Login',
+                    text: 'Create Account',
                     onTap: () {
                       if (_formKey.currentState!.validate()) {
                         Navigator.of(context).pushNamed(AppRoutes.homeRoute);
@@ -87,10 +87,9 @@ class _LoginPageState extends State<LoginPage> {
                       children: [
                         TextButton(
                           onPressed: () {
-                            Navigator.of(context)
-                                .pushNamed(AppRoutes.registerRoute);
+                            Navigator.of(context).pop();
                           },
-                          child: const Text('Don\'t have an account? Register'),
+                          child: const Text('You have an account? Login'),
                         ),
                         const SizedBox(height: 8),
                         Text(
@@ -102,14 +101,14 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                         const SizedBox(height: 16),
                         SocialMediaButton(
-                          text: 'Login with Google',
+                          text: 'Signup with Google',
                           imgUrl:
                               'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png',
                           onTap: () {},
                         ),
                         const SizedBox(height: 16),
                         SocialMediaButton(
-                          text: 'Login with Facebook',
+                          text: 'Signup with Facebook',
                           imgUrl:
                               'https://www.freepnglogos.com/uploads/facebook-logo-icon/facebook-logo-icon-facebook-logo-png-transparent-svg-vector-bie-supply-15.png',
                           onTap: () {},
