9056af2658446c59c9da7033a1016c1532d4f24a
Tarek Alabd <tarekalabd1@gmail.com>
2024-08-10 22:13:49 +0300

    feat: fetch the products from firestore (#44)

---
+++ lib/services/home_services.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_ecommerce_app/models/product_item_model.dart';
+import 'package:flutter_ecommerce_app/services/firestore_services.dart';
+import 'package:flutter_ecommerce_app/utils/api_paths.dart';
+
+abstract class HomeServices {
+  Future<List<ProductItemModel>> fetchProducts();
+}
+
+class HomeServicesImpl implements HomeServices {
+  final firestoreServices = FirestoreServices.instance;
+
+  @override
+  Future<List<ProductItemModel>> fetchProducts() async {
+    final result = await firestoreServices.getCollection<ProductItemModel>(
+      path: ApiPaths.products(),
+      builder: (data, documentId) => ProductItemModel.fromMap(
+        data,
+        documentId,
+      ),
+    );
+    return result;
+  }
+}
--- lib/view_models/home_cubit/home_cubit.dart
+++ lib/view_models/home_cubit/home_cubit.dart
@@ -1,19 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/models/home_carosel_item_model.dart';
 import 'package:flutter_ecommerce_app/models/product_item_model.dart';
+import 'package:flutter_ecommerce_app/services/home_services.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
 
-  void getHomeData() {
+  final homeServices = HomeServicesImpl();
+
+  Future<void> getHomeData() async {
     emit(HomeLoading());
-    Future.delayed(const Duration(seconds: 1), () {
+    try {
+      final products = await homeServices.fetchProducts();
+      final carouselItems = dummyHomeCarouselItems;
+      
       emit(HomeLoaded(
-        carouselItems: dummyHomeCarouselItems,
-        products: dummyProducts,
+        carouselItems: carouselItems,
+        products: products,
       ));
-    });
+    } catch (e) {
+      emit(HomeError(e.toString()));
+    }
+    // Future.delayed(const Duration(seconds: 1), () {
+    //   emit(HomeLoaded(
+    //     carouselItems: dummyHomeCarouselItems,
+    //     products: dummyProducts,
+    //   ));
+    // });
   }
 }
