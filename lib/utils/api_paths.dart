fb4b84e641acbc54179f2199addda7346936fa4e
Tarek Alabd <tarekalabd1@gmail.com>
2024-08-12 06:37:21 +0300

    feat: adding the favorite functionality

--- lib/utils/api_paths.dart
+++ lib/utils/api_paths.dart
@@ -1,6 +1,8 @@
 class ApiPaths {
   static String users(String userId) => 'users/$userId'; // users/$userId
   static String cartItem(String userId, String cartItemId) => 'users/$userId/cart/$cartItemId';
+  static String favoriteProduct(String userId, String productId) => 'users/$userId/favorites/$productId';
+  static String favoriteProducts(String userId) => 'users/$userId/favorites/';
   static String products() => 'products/';
   static String announcements() => 'announcements/';
   static String categories() => 'categories/';
--- lib/view_models/home_cubit/home_cubit.dart
+++ lib/view_models/home_cubit/home_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/models/home_carosel_item_model.dart';
 import 'package:flutter_ecommerce_app/models/product_item_model.dart';
+import 'package:flutter_ecommerce_app/services/auth_services.dart';
 import 'package:flutter_ecommerce_app/services/home_services.dart';
 
 part 'home_state.dart';
@@ -9,13 +10,14 @@ class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
 
   final homeServices = HomeServicesImpl();
+  final authServices = AuthServicesImpl();
 
   Future<void> getHomeData() async {
     emit(HomeLoading());
     try {
       final products = await homeServices.fetchProducts();
       final carouselItems = await homeServices.fetchCarouselItems();
-      
+
       emit(HomeLoaded(
         carouselItems: carouselItems,
         products: products,
@@ -23,11 +25,32 @@ class HomeCubit extends Cubit<HomeState> {
     } catch (e) {
       emit(HomeError(e.toString()));
     }
-    // Future.delayed(const Duration(seconds: 1), () {
-    //   emit(HomeLoaded(
-    //     carouselItems: dummyHomeCarouselItems,
-    //     products: dummyProducts,
-    //   ));
-    // });
+  }
+
+  Future<void> setFavorite(ProductItemModel product) async {
+    emit(SetFavoriteLoading());
+    try {
+      final currentUser = authServices.currentUser();
+      final favoriteProducts = await homeServices.fetchFavoriteProducts(
+        currentUser!.uid,
+      );
+      final isFavorite = favoriteProducts.any(
+        (item) => item.id == product.id,
+      );
+      if (isFavorite) {
+        await homeServices.removeFavoriteProduct(
+          userId: currentUser.uid,
+          productId: product.id,
+        );
+      } else {
+        await homeServices.addFavoriteProduct(
+          userId: currentUser.uid,
+          product: product,
+        );
+      }
+      emit(SetFavoriteSuccess(isFavorite: !isFavorite));
+    } catch (e) {
+      emit(SetFavoriteError(e.toString()));
+    }
   }
 }
