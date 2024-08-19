5bdfbcf699982fc4d75d42860bc178cc1af0bb45
Tarek Alabd <tarekalabd1@gmail.com>
2024-08-19 08:16:08 +0300

    feat: fetch cart items (#51)

--- lib/utils/api_paths.dart
+++ lib/utils/api_paths.dart
@@ -1,6 +1,7 @@
 class ApiPaths {
   static String users(String userId) => 'users/$userId'; // users/$userId
   static String cartItem(String userId, String cartItemId) => 'users/$userId/cart/$cartItemId';
+  static String cartItems(String userId) => 'users/$userId/cart/';
   static String favoriteProduct(String userId, String productId) => 'users/$userId/favorites/$productId';
   static String favoriteProducts(String userId) => 'users/$userId/favorites/';
   static String products() => 'products/';
