540d6cfb12c467d8bbce413d08c0f156ebc2eb11
Tarek Alabd <tarekalabd1@gmail.com>
2024-08-19 08:46:52 +0300

    53.increment decrement counter (#52)
    * feat: fetch cart items
    
    * feat: increment and decrement the counter logic

--- lib/services/cart_services.dart
+++ lib/services/cart_services.dart
@@ -4,6 +4,7 @@ import 'package:flutter_ecommerce_app/utils/api_paths.dart';
 
 abstract class CartServices {
   Future<List<AddToCartModel>> fetchCartItems(String userId);
+  Future<void> setCartItem(String userId, AddToCartModel cartItem);
 }
 
 class CartServicesImpl implements CartServices {
@@ -14,5 +15,12 @@ class CartServicesImpl implements CartServices {
       await firestoreServices.getCollection(
         path: ApiPaths.cartItems(userId),
         builder: (data, documentId) => AddToCartModel.fromMap(data),
+      );
+
+  @override
+  Future<void> setCartItem(String userId, AddToCartModel cartItem) async =>
+      await firestoreServices.setData(
+        path: ApiPaths.cartItem(userId, cartItem.id),
+        data: cartItem.toMap(),
       );
 }
