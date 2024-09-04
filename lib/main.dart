98380ededf75240de2ecbd085a46dbd59d3467d7
Tarek Alabd <tarekalabd1@gmail.com>
2024-09-04 08:37:37 +0300

    feat: handling interactions with notifications (#60)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/utils/app_router.dart';
@@ -6,10 +7,74 @@ import 'package:flutter_ecommerce_app/utils/app_routes.dart';
 import 'package:flutter_ecommerce_app/view_models/auth_cubit/auth_cubit.dart';
 import 'package:flutter_ecommerce_app/view_models/favorite_cubit/favorite_cubit.dart';
 
-void main() async {
+final navigatorKey = GlobalKey<NavigatorState>();
+
+Future<void> main() async {
+  await initializeApp();
+  runApp(const MyApp());
+}
+
+Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  await handleNotification();
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  debugPrint("Handling a background message: ${message.messageId}");
+}
+
+Future<void> handleNotification() async {
+
+  // Handling background messages
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Taking permission
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission();
+
+  debugPrint('User granted permission: ${settings.authorizationStatus}');
+
+  // Handling foreground messages
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    debugPrint('Got a message whilst in the foreground!');
+    debugPrint('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      String title = message.notification!.title ?? '';
+      String body = message.notification!.body ?? '';
+      debugPrint('Message also contained a notification: Title: $title');
+      debugPrint('Message also contained a notification: Body: $body');
+
+      showDialog(context: navigatorKey.currentContext!, builder: (_) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(body),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(navigatorKey.currentContext!).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },);
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    debugPrint('A new onMessageOpenedApp event was published!');
+    debugPrint('Message data: ${message.data}');
+    final messageData = message.data;
+    if (messageData['product_id'] != null) {
+      navigatorKey.currentState!.pushNamed(
+        AppRoutes.productDetailsRoute,
+        arguments: messageData['product_id'],
+      );
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -46,6 +111,7 @@ class MyApp extends StatelessWidget {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'E-commerce App',
+              navigatorKey: navigatorKey,
               theme: ThemeData(
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
